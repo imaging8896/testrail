892c2845f1f3aa34af8a993346ef8e136bbbb733
Ted Chen <tedchen@KKStreamde-MacBook-Pro.local>
2017-10-17 15:39:31 +0800

    Rename pypi package name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pytest-testrail',
-    version='1.1',
+    name='pytest-testrail-reporter',
+    version='1.2',
     packages=find_packages(exclude=['tests']),
     entry_points={
         'pytest11': [
-            'testrailutil = pytest_testrailutil.plugin',
+            'testrail = pytest_testrailutil.plugin',
         ]
     },
     install_requires=['setuptools', 'testrail', 'configparser==3.5.0'],
